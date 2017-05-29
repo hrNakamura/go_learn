9541d49c3b2c423a1b281310153bc533c198ab62
Hiroshi Nakamura <hiroshi.nh.nakamura@jrits.ricoh.co.jp>
2017-05-29 14:37:56 +0900

    練習問題4コメント追加

--- ch04/ex06/main.go
+++ ch04/ex06/main.go
@@ -7,20 +7,20 @@ import (
 )
 
 func main() {
-	a := []byte("abcd　　ef  g")
-	fmt.Println(string(a))
+	a := []byte("あいう　　ef  　g")
+	fmt.Printf("%q\n", string(a))
 	a = remSpace(a)
-	fmt.Println(string(a))
+	fmt.Printf("%q\n", string(a))
 }
 
 func remSpace(s []byte) []byte {
-	for i := 0; i < utf8.RuneCount(s); i++ {
+	for i := 0; i < len(s); i++ {
 		if utf8.RuneStart(s[i]) {
 			if r, size := utf8.DecodeRune(s[i:]); unicode.IsSpace(r) {
-				if r2, _ := utf8.DecodeRune(s[i+size:]); unicode.IsSpace(r2) {
-					copy(s[i+size:], s[i+size*2:])
+				if r2, size2 := utf8.DecodeRune(s[i+size:]); unicode.IsSpace(r2) {
+					copy(s[i+size:], s[i+size+size2:])
 					d := size
-					if size > 1 {
+					if size != 1 {
 						copy(s[i:], s[i+(size-1):])
 						s[i] = byte(32) //全角スペースの場合は半角スペースに置換
 						d += (size - 1)
